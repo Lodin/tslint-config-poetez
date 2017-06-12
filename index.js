d27f60d6f03f9bf8848ca40b68bfbb27528c1841
Vlad Rindevich <rindevich.vs@gmail.com>
2017-06-12 15:00:46 +0300

    refactor: remove tslint basic rules from "tslint-microsoft-contrib"

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-module.exports = require('./tslint.json');
+module.exports = {
+  extend: ['tslint:recommended', "tslint-eslint-rules"],
+  rules: Object.assign({}, require('./tslint-microsoft-contrib-recommended'), {
+    "completed-docs": false,
+    "missing-jsdoc": false,
+    "import-name": false,
+    "interface-name": false,
+    "mocha-no-side-effect-code": [true, {"ignore": "^xit.*"}],
+    "no-backbone-get-set-outside-model": false,
+    "no-default-export": false,
+    "no-import-side-effect": false,
+    "no-parameter-properties": false,
+    "no-relative-imports": false,
+    "no-unused-expression": [true, "allow-fast-null-checks"],
+    "prefer-array-literal": false,
+    "react-tsx-curly-spacing": [true, "never"],
+    "strict-boolean-expressions": false,
+    "typedef": [
+      true,
+      "call-signature",
+      "parameter"
+    ],
+    "quotemark": [
+      true,
+      "single",
+      "avoid-escape"
+    ]
+  }),
+};
